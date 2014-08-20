1fce1be7e72a9e6648dfc55aaa383d308338289c
Adam Barnhard <admbarn@gmail.com>
2014-08-20 16:25:41 -0500

    added geocoding

--- app/static/js/user/new-treasures.js
+++ app/static/js/user/new-treasures.js
@@ -1,8 +1,12 @@
+/* jshint unused:false, camelcase:false */
+/* global AmCharts:true, google:true */
+
 (function(){
   'use strict';
 
   $(document).ready(function(){
     $('#addHint').click(addHint);
+    $('form').submit(addTreasure);
   });
 
   function addHint(){
@@ -12,6 +16,37 @@
     $i.addClass('form-control');
     $('#hints').append($i);
   }
+
+  function addTreasure(e){
+    var lat = $('input[data-loc=lat]').val();
+
+    if(!lat){
+      var name = $('input[data-loc=name]').val();
+      geocode(name);
+      e.preventDefault();
+    }
+  }
+
+  function geocode(address){
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address: address}, function(results, status){
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      var name = results[0].formatted_address,
+          lat = results[0].geometry.location.lat(),
+          lng = results[0].geometry.location.lng();
+      console.log(lat, lng, name);
+      $('input[data-loc=name]').val(name);
+      $('input[data-loc=lat]').val(lat);
+      $('input[data-loc=lng]').val(lng);
+
+      // var data = $('form').serialize();
+      // console.log(data);
+
+      $('form').submit();
+
+    });
+  }
+
 })();
 
 
