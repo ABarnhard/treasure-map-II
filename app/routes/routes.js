5ab823fc38d05aaacb33807ad2dc4ef2f9ea31d3
Adam Barnhard <admbarn@gmail.com>
2014-08-21 15:18:08 -0500

    added styling

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,11 +4,13 @@ var morgan         = require('morgan'),
     bodyParser     = require('body-parser'),
     methodOverride = require('express-method-override'),
     less           = require('less-middleware'),
+    favicon        = require('serve-favicon'),
     treasures      = require('../controllers/treasures'),
     home           = require('../controllers/home');
 
 module.exports = function(app, express){
   app.use(morgan('dev'));
+  app.use(favicon(__dirname + '/../static/img/favicon.ico'));
   app.use(less(__dirname + '/../static'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
